a7666beaa817e43b5ca68178f233219f5cca7c40
JamesLiao <vivlonglz@gmail.com>
2020-11-27 14:24:50 +0800

    fix

--- config/app.php
+++ config/app.php
@@ -1,9 +1,12 @@
 <?php
 
-return array(
-    'Woocommerce' => array(
+return [
+    /*
+     * 相关配置
+    */
+    'Woocommerce' => [
         'site_url' => '<yourSiteUrl>',
         'consumer_key' => '<yourConsumerKey>',
         'consumer_secret' => '<yourConsumerSecret>',
-    ),
-);
+    ],
+];
