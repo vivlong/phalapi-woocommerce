a56f9e2743d294dfa82f128c40ae223e742b91b6
JamesLiao <vivlonglz@gmail.com>
2020-12-09 23:34:14 +0800

    update

--- src/Lite.php
+++ src/Lite.php
@@ -34,7 +34,7 @@ class Lite
             );
             $this->instance = $woocommerce;
         } catch (Exception $e) {
-            $di->logger->error($e->getMessage());
+            $di->logger->error('Woocommerce', ['error' => $e->getMessage()]);
         }
     }
 
@@ -43,13 +43,23 @@ class Lite
         return $this->instance;
     }
 
-    public function addProduct($parameters)
+    private function request($method = 'get', $route = '/', $parameters = [])
     {
         $di = \PhalApi\DI();
         $woocommerce = $this->instance;
         if (!empty($woocommerce)) {
             try {
-                $results = $woocommerce->post('products', $parameters);
+                $results = null;
+                switch ($method) {
+                    case 'post':
+                        $results = $woocommerce->post($route, $parameters);
+                        break;
+                    case 'delete':
+                        $results = $woocommerce->delete($route, $parameters);
+                        break;
+                    default:
+                        $results = $woocommerce->get($route, $parameters);
+                }
 
                 return $results;
             } catch (HttpClientException $e) {
@@ -59,139 +69,50 @@ class Lite
                 if ($rs && 400 == $rs->data->status) {
                     return $rs;
                 } else {
-                    $di->logger->error('Woocommerce # products request # '.$lastRequest->getBody());
-                    $di->logger->error('Woocommerce # products response # '.$lastResponse->getBody());
+                    $di->logger->error('Woocommerce', $method.' # '.$route, ['request' => $lastRequest->getBody()]);
+                    $di->logger->error('Woocommerce', $method.' # '.$route, ['response' => $lastResponse->getBody()]);
                 }
-                $di->logger->error('Woocommerce # products error # '.$e->getMessage());
+                $di->logger->error('Woocommerce', $method.' # '.$route, ['error' => $e->getMessage()]);
 
                 return null;
             }
         } else {
             return null;
         }
+    }
+
+    public function addProduct($parameters)
+    {
+        return $this->request('post', 'products', $parameters);
     }
 
     public function addCategories($data)
     {
-        $di = \PhalApi\DI();
-        $woocommerce = $this->instance;
-        if (!empty($woocommerce)) {
-            try {
-                $results = $woocommerce->post('products/categories', $data);
-
-                return $results->id;
-            } catch (HttpClientException $e) {
-                $lastRequest = $woocommerce->http->getRequest();
-                $lastResponse = $woocommerce->http->getResponse();
-                $rs = json_decode($lastResponse->getBody(), true);
-                if ($rs && 400 == $rs['data']['status'] && $rs['data']['resource_id'] > 0) {
-                    return $rs['data']['resource_id'];
-                } else {
-                    $di->logger->error('Woocommerce # addCategories request # '.$lastRequest->getBody());
-                    $di->logger->error('Woocommerce # addCategories response # '.$lastResponse->getBody());
-                }
-                $di->logger->error('Woocommerce # addCategories error # '.$e->getMessage());
-
-                return null;
-            }
-        } else {
-            return null;
-        }
+        return $this->request('post', 'products/categories', $parameters);
     }
 
     public function getProducts($parameters)
     {
-        $di = \PhalApi\DI();
-        $woocommerce = $this->instance;
-        if (!empty($woocommerce)) {
-            try {
-                $results = $woocommerce->get('products', $parameters);
-                $total = 0;
-                $totalPage = 0;
-                $lastResponse = $woocommerce->http->getResponse();
-                $headers = $lastResponse->getHeaders();
-                if (is_array($headers) && !empty($headers)) {
-                    $total = $headers['X-WP-Total'] ?? 0;
-                    $totalPage = $headers['X-WP-TotalPages'] ?? 0;
-                }
-
-                return [
-                    'items' => $results,
-                    'total' => intval($total),
-                    'totalPage' => intval($totalPage),
-                ];
-            } catch (HttpClientException $e) {
-                $lastRequest = $woocommerce->http->getRequest();
-                $lastResponse = $woocommerce->http->getResponse();
-                $rs = json_decode($lastResponse->getBody());
-                if ($rs && 400 == $rs->data->status) {
-                    return $rs;
-                } else {
-                    $di->logger->error('Woocommerce # getProducts request # '.$lastRequest->getBody());
-                    $di->logger->error('Woocommerce # getProducts response # '.$lastResponse->getBody());
-                }
-                $di->logger->error('Woocommerce # getProducts error # '.$e->getMessage());
-
-                return null;
-            }
-        } else {
-            return null;
-        }
+        return $this->request('get', 'products', $parameters);
     }
 
     public function getProduct($productId, $parameters)
     {
-        $di = \PhalApi\DI();
-        $woocommerce = $this->instance;
-        if (!empty($woocommerce)) {
-            try {
-                $results = $woocommerce->get('products/'.$productId, $parameters);
-
-                return $results;
-            } catch (HttpClientException $e) {
-                $lastRequest = $woocommerce->http->getRequest();
-                $lastResponse = $woocommerce->http->getResponse();
-                $rs = json_decode($lastResponse->getBody());
-                if ($rs && 400 == $rs->data->status) {
-                    return $rs;
-                } else {
-                    $di->logger->error('Woocommerce # getProduct request # '.$lastRequest->getBody());
-                    $di->logger->error('Woocommerce # getProduct response # '.$lastResponse->getBody());
-                }
-                $di->logger->error('Woocommerce # getProduct error # '.$e->getMessage());
-
-                return null;
-            }
-        } else {
-            return null;
-        }
+        return $this->request('get', 'products/'.$productId, $parameters);
     }
 
     public function deleteProduct($productId, $parameters)
     {
-        $di = \PhalApi\DI();
-        $woocommerce = $this->instance;
-        if (!empty($woocommerce)) {
-            try {
-                $results = $woocommerce->delete('products/'.$productId, $parameters);
+        return $this->request('delete', 'products/'.$productId, $parameters);
+    }
 
-                return $results;
-            } catch (HttpClientException $e) {
-                $lastRequest = $woocommerce->http->getRequest();
-                $lastResponse = $woocommerce->http->getResponse();
-                $rs = json_decode($lastResponse->getBody());
-                if ($rs && 400 == $rs->data->status) {
-                    return $rs;
-                } else {
-                    $di->logger->error('Woocommerce # deleteProduct request # '.$lastRequest->getBody());
-                    $di->logger->error('Woocommerce # deleteProduct response # '.$lastResponse->getBody());
-                }
-                $di->logger->error('Woocommerce # deleteProduct error # '.$e->getMessage());
+    public function post($route, $parameters)
+    {
+        return $this->request('post', $route, $parameters);
+    }
 
-                return null;
-            }
-        } else {
-            return null;
-        }
+    public function get($route, $parameters)
+    {
+        return $this->request('get', $route, $parameters);
     }
 }
