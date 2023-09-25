a61b3c84fd6d4856cc3e3dfc4e98b8034fb4ed8e
JamesLiao <vivlonglz@gmail.com>
2023-09-25 11:52:41 +0800

    update

--- src/Base.php
+++ src/Base.php
@@ -17,6 +17,7 @@ abstract class Base
     {
         $di = \PhalApi\DI();
         $woocommerce = $this->instance;
+        $logBase = __NAMESPACE__ . DIRECTORY_SEPARATOR . __CLASS__ . DIRECTORY_SEPARATOR . __FUNCTION__ . ' # ' . $method . ' # ' . $route;
         if (!empty($woocommerce)) {
             try {
                 $results = null;
@@ -34,16 +35,26 @@ abstract class Base
                         if ($returnArray) {
                             $total = 0;
                             $totalPage = 0;
+                            $queries = 0;
+                            $seconds = 0;
+                            $memory = 0;
                             $lastResponse = $woocommerce->http->getResponse();
                             $headers = $lastResponse->getHeaders();
                             if (is_array($headers) && !empty($headers)) {
-                                $total = $headers['X-WP-Total'] ?? 0;
-                                $totalPage = $headers['X-WP-TotalPages'] ?? 0;
+                                // $di->logger->info($logBase . ' # headers '. $headers['x-wp-total']);
+                                $total = $headers['X-WP-Total'] ?? $headers['x-wp-total'] ?? $headers['X-WP-TOTAL'] ?? 0;
+                                $totalPage = $headers['X-WP-TotalPages'] ?? $headers['x-wp-totalpages'] ?? $headers['X-WP-TOTALPAGES'] ?? 0;
+                                $queries = $headers['X-WP-Queries'] ?? $headers['x-wp-queries'] ?? $headers['X-WP-QUERIES'] ?? 0;
+                                $seconds = $headers['X-WP-Seconds'] ?? $headers['x-wp-seconds'] ?? $headers['X-WP-SECONDS'] ?? 0;
+                                $memory = $headers['X-WP-Memory'] ?? $headers['x-wp-memory'] ?? $headers['X-WP-MEMORY'] ?? 0;
                             }
                             $results = [
                                 'items' => $rs,
                                 'total' => intval($total),
                                 'totalPage' => intval($totalPage),
+                                'queries' => $queries,
+                                'seconds' => $seconds,
+                                'memory' => $memory,
                             ];
                         } else {
                             $results = $rs;
